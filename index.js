89c156a1fa4328c97c3b925e39ac971f24b922e2
Mikhail Goncharov <mgoncharov@arrow.com>
2018-01-26 00:03:30 -0700

    Changes to Handle Multiple actions

--- index.js
+++ index.js
@@ -16,26 +16,71 @@ restService.use(bodyParser.json());
 //enable best buy connection
 var bby = require('bestbuy')('ozDm81EOVzjYE0Rm8Ovan7aL');
 
-//sample to get information about products from Best Buy Site
-restService.post("/item", function(req, res) {
 
- var search = bby.products('sku=' + req.body.result.parameters.sku);
+//get information about products from Best Buy Site
+restService.post("/assistant", function(req, res) {
 
-  search
-  .then(function (data) {
+  switch (req.body.result.action) {
 
-      var speech = getItemInfo(data) ? getItemInfo(data) : "Seems like some problem. Speak again.";
+    //item details
+    case "item.details" :
 
-       // return to caller
+       var search = bby.products('sku=' + req.body.result.parameters.sku);
+
+        search.then(function (data) {
+
+            var speech = getItemInfo(data) ? getItemInfo(data) : "Seems like some problem. Speak again.";      
+            sendResultsToFlow(res, speech);
+        
+          });
+
+      break;
+
+
+    //case availability
+    case "item.details.availability" :
+
+      var sku = req.body.result.parameters.sku;
+      var zipcode = req.body.result.parameters.zipcode;
+
+      bby.realTimeAvailability(sku, {postalCode: zipcode}, function (err, data) {
+
+        if (err) console.error(err);
+        
+
+         if(data.stores.length > 0) {
+
+           var storeList = data.stores.map(s => `${s.name}`).join(',');
+           console.log(storeList);
+
+            var speech = "Yes, this Item is avilable in " + data.stores.length + " stores in this area";
+         } else {
+           var speech = "Sorry, this item is not available at this location"
+         }
+
+          sendResultsToFlow(res, speech);
+             
+      });
+
+      break;
+
+   }
+
+ 
+ });
+
+ 
+   //post results back to Flow
+   function sendResultsToFlow(res, results) {
+
+     // return to caller
         return res.json({
-          speech: speech,
-          displayText: speech,
+          speech: results,
+          displayText: results,
           source: "webhook-arrow-testing"
         });
-  
-    });
 
-   });
+   }
 
   //get item informartion from best buy
   function getItemInfo (data) {
